6c95371436b2bcb974aed5cb1b515377a06342c7
Austin Metke <metke70.am@gmail.com>
2022-02-12 12:23:37 -0800

    Improved UI, Form() is more readable, added company logo to be displayed on login

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:dbcrypt/dbcrypt.dart';
 import 'package:flutter/services.dart';
 
-
 Future<void> main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
@@ -15,29 +13,15 @@ Future<void> main() async {
   );
 
   runApp(ExpenseTracker());
-
 }
-
 
 class ExpenseTracker extends StatelessWidget {
   const ExpenseTracker({Key? key}) : super(key: key);
 
-
-
   @override
   Widget build(BuildContext context) {
-
     return const MaterialApp(
-
-      home: Scaffold(
-
-        resizeToAvoidBottomInset: false,
-        body: LoginPage()
-      )
-
-    );
-
-
+        home: Scaffold(resizeToAvoidBottomInset: false, body: LoginPage()));
   }
 }
 
@@ -49,7 +33,6 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
   final _key = GlobalKey<FormState>();
 
   final phoneNumberRegex = RegExp(r'^[0-9]*$');
@@ -61,197 +44,154 @@ class _LoginPageState extends State<LoginPage> {
 
   late bool _visPass = false;
 
-
-
-
-
- _submitForm() async {
-
+  _submitForm() async {
     final formState = _key.currentState;
 
-    if(formState!.validate()) {
-
-
-
+    if (formState!.validate()) {
       print("Valid form! $_phoneNumber $_password");
-
-
     }
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Center(
 
-      widthFactor: double.infinity,
-      heightFactor: 3.25,
-      child: Form(
-
+        child: Form(
           key: _key,
-
           child: SingleChildScrollView(
-
             child: Column(
+             // mainAxisAlignment: MainAxisAlignment.center,
+              //crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
 
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-
-            children: [
-
-
-
-
-              //Phone number field
-              TextFormField(
-
-                decoration: const InputDecoration(
-
-                  prefixIcon: Icon(Icons.dialpad_outlined),
-
-                  hintText: "Phone number",
-
-                  contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 20, 0, 100),
+               child: Image.asset('./assets/cvcenterprise.png'),
                 ),
-
-                validator: (value) {
-
-                  if(!phoneNumberRegex.hasMatch(value!) || value.isEmpty || value.length < phoneNumberLength) {
-
-                    return 'Please enter a valid phone number';
-                  }
-
-                  //return null if text is valid
-                  return null;
-
-                },
-
-
-                onChanged: (value) => _phoneNumber = value,
-
-
-                keyboardType: TextInputType.phone,
-                inputFormatters: [
-                  FilteringTextInputFormatter(
-                      phoneNumberRegex,
-                      allow: true),
-                  LengthLimitingTextInputFormatter(phoneNumberLength)
-                ],
-
-              ),
-
-
-
-              //Password Field
-              TextFormField(
-
-                decoration: InputDecoration(
-
-                    prefixIcon: Icon(Icons.lock),
-                    hintText: "Password",
-                    suffixIcon: GestureDetector(
-
-                      child: _visPass ? const Icon(Icons.visibility_off) : const Icon(Icons.visibility),
-
-                      onTap: () => setState(() {
-                        _visPass = !_visPass;
-                      })
-                    ),
-
-                    contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-
-
-              ),
-
-                validator: (value) {
-
-                  if (value!.isEmpty) {
-
-                    return 'Please enter your password';
-
-                  }
-
-                  //return null if text is valid
-                  return null;
-
-                },
-
-                obscureText: !_visPass,
-
-                onChanged: (value) => _password = value,
-
-
-              ),
-
-
-
-              TextButton(
-
-                style: ButtonStyle(
-
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.lightGreen),
-                  foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-
+                //Phone number field
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: phoneNumberField(),
                 ),
 
 
-                onPressed: () {_submitForm();}, child: const Text("Login"),
+                //Password Field
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: passwordField(),
+                ),
 
-              )
-
-
-            ],
-
+                TextButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.lightGreen),
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                  ),
+                  onPressed: () => _submitForm(),
+                  child: const Text("Login"),
+                )
+              ],
+            ),
           ),
-
-
-
-        ),
-
-
-      )
-    );
-
-
+        ));
   }
 
 
+
+
+
+  Widget passwordField() => TextFormField(
+    decoration: InputDecoration(
+      labelText: "Password",
+      border: const OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      prefixIcon: const Icon(Icons.lock),
+      hintText: "Password",
+      suffixIcon: GestureDetector(
+           child: _visPass ? const Icon(Icons.visibility_off) : const Icon(Icons.visibility),
+          onTap: () => setState(() {
+            _visPass = !_visPass;
+          })),
+    ),
+    validator: (value) {
+      if (value!.isEmpty) {
+        return 'Please enter your password';
+      }
+
+      //return null if text is valid
+      return null;
+    },
+    obscureText: !_visPass,
+    onChanged: (value) => _password = value,
+    onFieldSubmitted: (value) {if(!_key.currentState!.validate()){
+
+      print("Invalid password");
+
+    }
+    },
+  );
+
+
+  Widget phoneNumberField() => TextFormField(
+    decoration: const InputDecoration(
+      labelText: "Phone number",
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      prefixIcon: Icon(Icons.dialpad_outlined),
+      hintText: "Phone number",
+    ),
+
+    validator: (value) {
+      if (!phoneNumberRegex.hasMatch(value!) ||
+          value.isEmpty ||
+          value.length < phoneNumberLength) {
+        return 'Please enter a valid phone number';
+      }
+      //return null if text is valid
+      return null;
+    },
+
+    onChanged: (value) => _phoneNumber = value,
+    onFieldSubmitted: (value) {
+
+      if(!_key.currentState!.validate()) {
+        print("Invalid phone number");
+      }
+
+    },
+    keyboardType: TextInputType.phone,
+    inputFormatters: [
+      FilteringTextInputFormatter(phoneNumberRegex,
+          allow: true),
+      LengthLimitingTextInputFormatter(phoneNumberLength)
+    ],
+  );
+
 }
 
-class User {
 
+class User {
   var _username;
   var _passwordHash;
 
   createUser(String username, String password) {
-
     _username = username;
     _passwordHash = DBCrypt().hashpw(password, DBCrypt().gensalt());
-
-
   }
 
   delUser(User user) {
-
     user._passwordHash = null;
     user._username = null;
-
   }
 
   getHash() {
-
     return _passwordHash;
-
   }
 
   getUsername() {
     return _username;
   }
-
-
-
-
-
-
 }
